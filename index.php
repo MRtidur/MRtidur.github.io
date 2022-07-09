43adbe6989a387fd367f9a34bb81d9e454a93691
zulfikarhadi <zulfikarhadi31@gmail.com>
2022-07-09 21:27:15 +0700

    add new file

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <div class="navbar" style="position: fixed; z-index: 999;"><?php
+                                                                include 'header.php';
+                                                                ?>
+    </div>
+    <title>Document</title>
+</head>
+
+<body style="background-color:#202121;">
+    <div class="container1">
+        <div class="text">
+            <div class="cover">
+                <h1>PORTFOLIO <br> ZULFIKAR HADI</h1>
+                <h2>lksafoaskmkfasuifas9fj9aposkfkaspofaopjfihu
+                    <br>
+                    asifoiasjfjasiofaisokfskaofji9aifoskjaojfoas
+                    <br>
+                    asofjoaisfjiosajifjoajifjiasji
+                </h2>
+            </div>
+        </div>
+    </div>
+    <div class="desc">
+        <div class="profile"></div>
+        <h1>ABOUT ME</h1>
+        <h2>Let me Introduce My self,My name Zulfikar Hadi im living in Batam City,Indonesia
+            i am Programer graduated from SMKN 7 Batam Majoring in Software Enginer,and study at Universitas Terbuka Majoring in System Information.beside being programer i also like to draw like Digital Drawing & paper drawing.
+        </h2>
+    </div>
+    <div class="container2">
+
+        <div class="satelit"></div>
+        <div class="menu">
+            <div class="project">
+
+                <a href="menu/project.php">
+                    <div class="cover-menu">
+                        <h1>PROJECT</h1>
+                        <h2>1.E-Commerce<br>
+                            2.Art Gallery <br>
+                            3.Old Portfolio <br>
+                        </h2>
+                    </div>
+                </a>
+
+
+            </div>
+            <div class="art">
+                <a href="">
+                    <div class="cover-menu">
+                        <h1>ART</h1>
+                        <h2>slfjaosfasjfiojasidjfioj</h2>
+                    </div>
+                </a>
+
+            </div>
+            <div class="food">
+                <a href="">
+                    <div class="cover-menu">
+                        <h1>FOOD</h1>
+                        <h2>shmdetach'askpjasof</h2>
+                    </div>
+                </a>
+
+            </div>
+            <div class="photograph">
+                <a href="">
+                    <div class="cover-menu">
+                        <h1>PHOTOGRAPH</h1>
+                        <h2>jfiojasifhuiashu</h2>
+                    </div>
+                </a>
+
+            </div>
+        </div>
+
+</body>
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+</html>
